ae9dd72c578c7bb929a6d5c443dc806ba2ee9c1d
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-02-26 03:55:39 +0000

    payment till formula is done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,49 @@ class ApplicationController < ActionController::Base
   end
 def calculate_square
   #Parameters: {"elephant"=>"42"}
-  @num = params.fetch("elephant").to_f
+  @num = params.fetch("number").to_f
   @square_of_num = @num ** 2
    render({:template => "calculation_templates/square_results.html.erb"})
 end
+def blank_square_root_form
+
+
+  render({:template => "calculation_templates/square_root_form.html.erb" })
+  end
+def calculate_square_root
+  #Parameters: {"elephant"=>"42"}
+  @num = params.fetch("user_number").to_f
+  @square_root_of_num = Math.sqrt(@num)
+   render({:template => "calculation_templates/square_root_results.html.erb"})
+end
+def blank_calculate_random
+
+  render({:template => "calculation_templates/rand_new.html.erb" })
+  end
+def calculate_random
+  #Parameters: {"elephant"=>"42"}
+  @lower = params.fetch("user_min").to_f
+  @upper = params.fetch("user_max").to_f
+
+  @result = rand(@lower..@upper)
+   render({:template => "calculation_templates/rand_results.html.erb"})
+end
+def blank_calculate_payment
+
+  render({:template => "calculation_templates/payment_new.html.erb" })
+  end
+def calculate_payment
+  #Parameters: {"elephant"=>"42"}
+  @apr = params.fetch("user_apr").to_s
+  #to_s(:percentage, { :precision => 4 } ) 
+  #@ap= @apr(:percentage, { :precision => 4 })
+  @no_of_years = params.fetch("user_years").to_i
+  @principal = params.fetch("user_pv").to_f
+  
+  @numerator = ((@apr/12)*(@principal))
+  @denominator = 1- (1/((1+(@apr/12))**(@no_of_years*12)))
+  @payment = @numerator/@denominator
+   render({:template => "calculation_templates/payment_results.html.erb"})
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 get("/square/new",{:controller => "application",:action => "blank_square_form"})
 get("/square/results",{:controller => "application",:action => "calculate_square"})
+get("/square_root/new",{:controller => "application",:action => "blank_square_root_form"})
+get("/square_root/results",{:controller => "application",:action => "calculate_square_root"})
+get("/random/new",{:controller => "application", :action => "blank_calculate_random"})
+get("/random/results",{:controller => "application", :action => "calculate_random"})
+get("/payment/new",{:controller => "application", :action => "blank_calculate_payment"})
+get("/payment/results",{:controller => "application", :action => "calculate_payment"})
+
 end
